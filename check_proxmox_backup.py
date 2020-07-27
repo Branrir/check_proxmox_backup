a9bb378d84398d476dbd97e243bbcf3b1030ae1b
Branrir <alexey.sorokin.work@gmail.com>
2020-07-27 16:10:12 +0200

    no backups error fix

--- check_proxmox_backup.py
+++ check_proxmox_backup.py
@@ -78,7 +78,7 @@ def main(args):
 
         # Get date string
         if vmtype != None:
-            if backups[-1]:
+            if len(backups) != 0:
                 last_item = backups[-1]
                 size = round(int(last_item.rsplit(" ", 2)[-2][:-1])*9.3132257461548E-9, 2)
                 backup_date = re.search(r"\d{4}\_\d{2}\_\d{2}\-\d{2}\_\d{2}\_\d{2}", last_item).group()
